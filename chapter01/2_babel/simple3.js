cc78d3ec8428f9d531feb60f4fa6b750bd9f1b8f
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-06-05 01:43:34 +0900

    simple3.js 수정

--- chapter01/2_babel/simple3.js
+++ chapter01/2_babel/simple3.js
@@ -1,13 +1,38 @@
 function LikeButton() {
-    const [Liked, setLiked] = React.useState(false);
-    const test = Liked ? '좋아요 취소' : '좋아요';
-
-    return React.createElement(
-        'button',
-        { onClick: () => setLiked(!Liked)},
-        test
-    ); 
+  const [Liked, setLiked] = React.useState(false);
+  const test = Liked ? "좋아요 취소" : "좋아요";
+  return React.createElement(
+    "button",
+    { onClick: () => setLiked(!Liked) },
+    test
+  );
 }
 
-const domContainer = document.querySelector('#react-root');
-ReactDOM.render(React.createElement(LikeButton), domContainer);
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(LikeButton),
+    React.createElement(
+      "div",
+      { style: { marginTop: 20 } },
+      React.createElement("span", null, "현재 카운트: "),
+      React.createElement("span", null, count),
+      React.createElement(
+        "button",
+        { onClick: () => setCount(count + 1) },
+        "증가"
+      ),
+      React.createElement(
+        "button",
+        { onClick: () => setCount(count - 1) },
+        "감소"
+      )
+    )
+  );
+}
+
+const domContainer = document.querySelector("#react-root");
+console.log("domContainer: ", domContainer);
+ReactDOM.render(React.createElement(Container), domContainer);
