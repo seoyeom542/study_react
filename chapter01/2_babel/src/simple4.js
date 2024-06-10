692575825f703b98c76cebcea069f638414751ee
덩어리 <seoyeom542@deong-eoliui-iMac.local>
2024-06-10 20:20:20 +0900

    오타 수정

--- chapter01/2_babel/src/simple4.js
+++ chapter01/2_babel/src/simple4.js
@@ -16,7 +16,7 @@ function Container() {
       <LikeButton />
       <div sttle={{ marginTop: 20 }}>
         <span>현재 카운트: </span>
-        <spam>{count}</spam>
+        <span>{count}</span>
         <button onClick={() => setCount(count + 1)}>증가</button>
         <button onClick={() => setCount(count - 1)}>감소</button>
       </div>
